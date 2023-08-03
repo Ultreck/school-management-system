99c5c4ede2eca1fea4b13fbf1cedb1a296f6616a
Ultreck <adetutuemmanueloluwatayese@gmail.com>
2023-08-03 11:19:03 +0100

    reactUpdate

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,10 +1,8 @@
-// let baseUrl = 'https://school-management-app.onrender.com'
-// let baseUrl = 'https://school-management-app.onrender.com'
-    let  baseUrl = 'http://localhost:4000'
-// if(process.env.NODE_ENV.PORT === "production"){
-//       baseUrl = "https://school-management-app.onrender.com"
-// }
-// else{
-//       baseUrl = 'http://localhost:4000'
-// }
+let baseUrl = '';
+if(process.env.NODE_ENV.PORT === "production"){
+      baseUrl = "https://school-management-app.onrender.com"
+}
+else{
+      baseUrl = 'http://localhost:4000'
+}
 export {baseUrl};
